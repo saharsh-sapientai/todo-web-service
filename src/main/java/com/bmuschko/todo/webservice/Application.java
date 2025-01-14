30842628d5c27504dba925f0f51f62cd6bff58a3
Saharsh Basal <saharsh@sapient.ai>
2025-01-14 09:32:46 +0530

    Upgrade to java 17.

--- src/main/java/com/bmuschko/todo/webservice/Application.java
+++ src/main/java/com/bmuschko/todo/webservice/Application.java
@@ -2,8 +2,10 @@ package com.bmuschko.todo.webservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+//@EntityScan("com.bmuschko.todo.webservice.model")
 public class Application {
 
     public static void main(String[] args) {
