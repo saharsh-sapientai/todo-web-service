c6a218baaff883ca5c0b612925d850c187d9cdf1
Saharsh Basal <saharsh@sapient.ai>
2025-01-28 12:39:13 +0530

    Added service name in environment variable.

--- run-simulation.sh
+++ run-simulation.sh
@@ -1,4 +1,5 @@
 export LE_SERVICE_URL=https://app.baserock.ai/it/leService
 export SERVICE_UNDER_TEST_URL=http://localhost:8080
+export SERVICE_NAME=todo-web-service
 
-./api_automation_binary todo-web-service ""
+./api_automation_binary ${SERVICE_NAME} ""
--- run-todo-web-service_simulate.sh
+++ run-todo-web-service_simulate.sh
@@ -1,16 +1,17 @@
 export ENABLE_EXTENSION=true;
 export EXTENSION_MODE=simulate;
 export GO_SERVER_BASE_URL=https://app.baserock.ai/it/leService;
+export SERVICE_NAME=todo-web-service;
+export PUBLISHER_MODE=RabbitMQ;
 export MQ_HOST=localhost;
 export MQ_PORT=5673;
 export MQ_QUEUE=learning_engine_test_queue;
 export MQ_USER=user;
-export PUBLISHER_MODE=RabbitMQ;
 export MQ_PASSWORD=<password>
 
 
 java -javaagent:/Users/saharshbasal/Development/git_main/testing/todo-web-service/src/main/resources/opentelemetry-javaagent.jar \
- -Dotel.resource.attributes=service.name=todo-web-service \
+ -Dotel.resource.attributes=service.name=${SERVICE_NAME} \
  -Dotel.javaagent.extensions=/Users/saharshbasal/Development/git_main/testing/todo-web-service/src/main/resources/sapient-otel-extension-1.0-all.jar \
  -Dio.opentelemetry.javaagent.slf4j.simpleLogger.defaultLogLevel=off \
  -Dspring.profiles.active=dev \
